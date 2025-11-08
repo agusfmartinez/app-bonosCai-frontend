06b0ec7f31ca4b25f0dc9b9834b35d3ab1ad92a0
agusfmartinez <agusfmartinez99@gmail.com>
2025-11-08 01:44:19 -0300

    update .env

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:4000',
+        target: process.env.API_URL || 'http://localhost:4000',
         changeOrigin: true,
         ws: true
       },
